054c027dd60da37ed5ad1f11c4514e44d9c6a2ac
Daniel Bachhuber <d@danielbachhuber.com>
2014-01-30 11:36:26 +0100

    MOAR SPEED

--- download-urls.php
+++ download-urls.php
@@ -1,6 +1,6 @@
 <?php
 
-$max_processes = 80;
+$max_processes = 100;
 
 $file = dirname( __FILE__ ) . '/top-1m.csv';
 $fp = fopen( $file, 'r' );
@@ -19,7 +19,7 @@ while ( ( $data = fgetcsv( $fp, 1000, "," ) ) !== false ) {
 	if ( ! file_exists( $site_file ) ) {
 		$spawned = true;
 		echo "[{$number}] Downloading: {$data[1]}" . PHP_EOL;
-		shell_exec( "wget -O {$site_file} -t 10 {$data[1]} > /dev/null 2>/dev/null &" );
+		shell_exec( "wget -O {$site_file} -t 4 {$data[1]} > /dev/null 2>/dev/null &" );
 	} else {
 		$spawned = false;
 		echo "[{$number}] File already exists: {$data[1]}" . PHP_EOL;
