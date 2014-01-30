dbce0e428bbc39228da90a170dd18463bf6b00fb
Daniel Bachhuber <d@danielbachhuber.com>
2014-01-30 10:53:42 +0100

    Do some waiting if we have too many processes

--- download-urls.php
+++ download-urls.php
@@ -1,9 +1,16 @@
 <?php
 
+$max_processes = 20;
+
 $file = dirname( __FILE__ ) . '/top-1m.csv';
 $fp = fopen( $file, 'r' );
 
 while ( ( $data = fgetcsv( $fp, 1000, "," ) ) !== false ) {
+
+	// Wait if there are too many processes
+	while( trim( shell_exec( 'ps aux | grep wget | wc -l' ) ) > $max_processes ) {
+		sleep( 1 );
+	}
 
 	$number = str_pad( $data[0], 7, 0, STR_PAD_LEFT );
 	$site_file = dirname( __FILE__ ) . '/data/' . $number . '.html';
